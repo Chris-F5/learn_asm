e12b550db6d3dbbf3ae794fb61905ba7f9eb2126
Christopher Lang <christopher.lang.256@gmail.com>
2023-07-30 15:16:47 +0100

    Build for i386

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: run clean
 
 hello.o: hello.asm
-	nasm -f elf64 hello.asm -o hello.o
+	nasm -f elf32 hello.asm -o hello.o
 hello: hello.o
 	ld -m elf_i386 hello.o -o hello
 run: hello
--- hello.asm
+++ hello.asm
@@ -1,14 +1,20 @@
 SECTION .data
 msg db 'hello world', 10 ; newline decimal
 
+; kernel interrupt registers
+; arch   call  arg1  arg2  arg3  arg4  arg5  arg6
+; -------------------------------------------------
+; i386   eax   ebx   ecx   edx   esi   edi   ebp
+; x86-64 rax   rdi   rsi   rdx   r10   r8    r9
+
 SECTION .text
 global  _start
 _start:
   ; print hello
-  mov edx, 12  ; byte count
-  mov ecx, msg ; bytes
-  mov ebx, 1   ; stdout
   mov eax, 4   ; SYS_WRITE
+  mov ebx, 1   ; stdout
+  mov ecx, msg ; bytes
+  mov edx, 12  ; byte count
   int 80h      ; kernel interrupt
 
   ; exit
